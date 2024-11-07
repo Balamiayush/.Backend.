b7d740532ef44680795d3289dc2a405442dbf2d6
Balamiayush <aryanbalami54@gmail.com>
2024-11-07 19:30:47 +0545

    hello

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,12 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const registerUser = asyncHandler(async(req,res)=>{
+    //get a details from frontend 
+    //validation -not empty
+    //check the user is already exitst
+    //check for images check for avatar
+    //upload them to cloudinary
+    //create user object 
+})
+
+export { registerUser }
