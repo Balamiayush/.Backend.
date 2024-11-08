a01ece710686d41fe6871a73856f3db9a62e78c6
Balamiayush <aryanbalami54@gmail.com>
2024-11-08 19:46:48 +0545

    login

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,12 +1,60 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+const registerUser = asyncHandler(async (req, res) => {
+  //get a details from frontend req.body
+  const { email, password, username, fullName } = req.body;
+  //validation -not empty
+  if (
+    [fullName, password, email, username].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+  //check the user is already exitst
+  const existedUser = User.findOne({
+    $or: [{ email }, { username }],
+  });
+  if (existedUser) {
+    throw new ApiError(400, "User already exists");
+  }
 
-const registerUser = asyncHandler(async(req,res)=>{
-    //get a details from frontend 
-    //validation -not empty
-    //check the user is already exitst
-    //check for images check for avatar
-    //upload them to cloudinary
-    //create user object 
-})
+  //check for images check for avatar
 
-export { registerUser }
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar is required");
+  }
+
+  //upload them to cloudinary
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+  if (!avatar) {
+    throw new ApiError(500, "Failed to upload avatar to cloudinary");
+  }
+
+  //create user object
+  const user = await User.create({
+    username,
+    email,
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    password,
+  });
+  const createdUser = await user
+    .fineById(user._id)
+    .select("-password -refreshToken");
+  if (!createdUser) {
+    throw new ApiError(500, "Failed to create user");
+  }
+  return res.status(201).json(
+    new ApiResponse(200,createdUser,"user register succesfully")
+  )
+});
+
+export { registerUser };
